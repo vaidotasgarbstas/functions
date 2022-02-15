dc776be5309a3e1e9486f15a30a39cd2c4aa61ff
vaidotasgarbstas <vaidotas.garbstas@gmail.com>
2022-02-15 18:34:25 +0200

    To-do list

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-function getKMI(w, h) {
-    return (w / (h / 100 * h / 100)).toFixed(2);
-}
+let form = document.querySelector("form");
+let ul = document.querySelector("ul");
 
-function info(index) {
-    let answer = "error";
-    if (index < 18.5) {
-        answer = "svoris nepakankamas";
-    } else if (index >= 18.5 && index < 25) {
-        answer = "svoris normalus";
-    } else if (index >= 25 && index <= 30) {
-        answer = "antsvoris";
-    } else if (index > 30) {
-        answer = "nutukimas";
-    } return answer;
-}
-
-let kmi = document.createElement("p")
-let calc = document.forms["calculator"];
-
-calc.addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    let weight = +calc["weight"].value;
-    let height = +calc["height"].value;
-    kmi.textContent = "Jusu KMI: " + getKMI(weight, height) + " - " + info(getKMI(weight, height));
-    body.appendChild(kmi);
-});
+    let listElement = form["toDo"].value;
+    let list = document.createElement("li");
+    list.textContent = listElement;
 
+    ul.appendChild(list);
+    e.target.elements.toDo.value = "";
+})
+
+
+
+const lista = document.querySelector('input[type="text"]');
+
+lista.addEventListener('focus', (event) => {
+    event.target.style.background = '#4CAF50';
+    // lista.value = "";
+});
+lista.addEventListener('blur', (event) => {
+    event.target.style.background = '';
+});
