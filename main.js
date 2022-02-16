594cc70cd085036b0dfa0dea8023b85a4b7324c8
vaidotasgarbstas <vaidotas.garbstas@gmail.com>
2022-02-16 09:04:11 +0200

    Drop-down menu

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-let form = document.querySelector("form");
-let ul = document.querySelector("ul");
+document.forms[0].addEventListener("submit", function (e) {
+    e.preventDefault()
+    let value = document.getElementById("mySelect").value;
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let listElement = form["toDo"].value;
-    let list = document.createElement("li");
-    list.textContent = listElement;
+    let season = document.getElementById("mySelect");
+    let body = document.querySelector("body");
 
-    ul.appendChild(list);
-    e.target.elements.toDo.value = "";
-})
-
-
-
-const lista = document.querySelector('input[type="text"]');
-
-lista.addEventListener('focus', (event) => {
-    event.target.style.background = '#4CAF50';
-    // lista.value = "";
-});
-lista.addEventListener('blur', (event) => {
-    event.target.style.background = '';
+    if (value == "vasara") {
+        body.style.backgroundImage = "url(https://content.thriveglobal.com/wp-content/uploads/2020/06/summer.jpg?w=1550)"
+    }
+    else if (value == "pavasaris") {
+        body.style.backgroundImage = "url(https://cdn.britannica.com/05/155405-050-F8969EE6/Spring-flowers-fruit-trees-bloom.jpg)";
+    }
+    else if (value == "ruduo") {
+        body.style.backgroundImage = "url(https://cdn.britannica.com/88/137188-050-8C779D64/Boston-Public-Garden.jpg)";
+    }
+    else if (value == "ziema") {
+        body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/b/b0/Winter_forest_silver.jpg)";
+    }
 });
