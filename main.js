42560c4fc65b1ea8cab25ef7b7f60aca0a411748
vaidotasgarbstas <vaidotas.garbstas@gmail.com>
2022-02-18 19:28:48 +0200

    API cards randomizer

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
-// document.querySelector("div").addEventListener("click", function (e) {
-//     console.log(e.target);
-//     console.log(e.target.alt);
-//     let imgSrc = e.target;
-//     let src = imgSrc.getAttribute("src");
-// })
+function createCard(imageURL, nameSurname, year, location, mail) {
+    let newCardDiv = document.createElement("div");
+    newCardDiv.className = "card";
+    let profImg = document.createElement("img");
+    profImg.className = "img";
+    profImg.src = imageURL;
+    let newName = document.createElement("div");
+    newName.textContent = nameSurname;
+    let newAge = document.createElement("div");
+    newAge.textContent = year;
+    let newAddress = document.createElement("div");
+    newAddress.textContent = location;
+    let newEmail = document.createElement("div");
+    newEmail.textContent = mail;
 
-function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+    newCardDiv.append(profImg, newName, newAge, newAddress, newEmail);
+    console.log(newCardDiv)
+    return newCardDiv
 }
+function appendCard(card) {
+    let parrent = document.querySelector(".container");
+    parrent.append(card);
+}
+
+async function getData() {
+    const requestURL = 'https://randomuser.me/api/';
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+    getData().then(data => {
+        // imageURL, nameSurname, year, location, mail
+        let { picture, name, dob, location, email } = data.results[0];
+        name = `${name.title} ${name.first} ${name.last}`;
+        location = `${location.city} ${location.country}`;
+        picture = picture.large;
+        email = email;
+        dob = dob.age;
+
+        let card = createCard(picture, name, dob, location, email)
+        appendCard(card);
+
+    }).catch(error => {
+        console.log(error);
+    });
+})
