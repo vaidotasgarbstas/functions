de17962413991f8f69db783bcfb9c24e515ae814
vaidotasgarbstas <vaidotas.garbstas@gmail.com>
2022-02-24 17:06:10 +0200

    Breaking Bad characters

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function createCard(imageURL, nameSurname, year, location, mail) {
+function createCard(imageURL, nameSurname, nick, position) {
     let newCardDiv = document.createElement("div");
     newCardDiv.className = "card";
     let profImg = document.createElement("img");
@@ -6,14 +6,13 @@ function createCard(imageURL, nameSurname, year, location, mail) {
     profImg.src = imageURL;
     let newName = document.createElement("div");
     newName.textContent = nameSurname;
-    let newAge = document.createElement("div");
-    newAge.textContent = year;
-    let newAddress = document.createElement("div");
-    newAddress.textContent = location;
-    let newEmail = document.createElement("div");
-    newEmail.textContent = mail;
+    let newNick = document.createElement("div");
+    newNick.textContent = nick;
+    let newPosition = document.createElement("div");
+    newPosition.textContent = position;
 
-    newCardDiv.append(profImg, newName, newAge, newAddress, newEmail);
+
+    newCardDiv.append(profImg, newName, newNick, newPosition);
     console.log(newCardDiv)
     return newCardDiv
 }
@@ -21,9 +20,10 @@ function appendCard(card) {
     let parrent = document.querySelector(".container");
     parrent.append(card);
 }
+//RANDOM CHARACTER-----------------------------------------------------------------
 
-async function getData() {
-    const requestURL = 'https://randomuser.me/api/';
+async function getRandom() {
+    const requestURL = 'https://www.breakingbadapi.com/api/character/random';
     const request = new Request(requestURL);
     const response = await fetch(request, { mode: 'cors' });
     const data = await response.json();
@@ -34,19 +34,191 @@ async function getData() {
 
 let btn = document.querySelector("button");
 btn.addEventListener("click", function () {
-    getData().then(data => {
-        // imageURL, nameSurname, year, location, mail
-        let { picture, name, dob, location, email } = data.results[0];
-        name = `${name.title} ${name.first} ${name.last}`;
-        location = `${location.city} ${location.country}`;
-        picture = picture.large;
-        email = email;
-        dob = dob.age;
-
-        let card = createCard(picture, name, dob, location, email)
+    getRandom().then(data => {
+        // (imageURL, nameSurname, nick, position)
+        let { img, name, nickname, occupation } = data[0];
+        let card = createCard(img, name, nickname, occupation)
         appendCard(card);
 
     }).catch(error => {
         console.log(error);
     });
+})
+//REMOVE ALL CHARACTERS------------------------------------------------------------
+let btnRemove = document.getElementById("remove");
+btnRemove.addEventListener("click", function () {
+    let reset = document.querySelector(".container");
+    reset.innerHTML = "";
+})
+
+//GET CHARACTER BY ID-----------------------------------------------------
+
+async function getCharacter(id) {
+    const requestURL = 'https://www.breakingbadapi.com/api/characters/' + id;
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btnID = document.getElementById("id");
+btnID.addEventListener("click", function () {
+    input = prompt("Enter ID", 1);
+
+    getCharacter(input).then(data => {
+        let { img, name, nickname, occupation } = data[0];
+        let card = createCard(img, name, nickname, occupation)
+        appendCard(card);
+
+    }).catch(error => {
+        console.log(error);
+    });
+})
+
+//GET ALL CHARACTERS-----------------------------------------------------------
+
+async function getAll(all) {
+    const requestURL = 'https://www.breakingbadapi.com/api/characters/';
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btnAll = document.getElementById("all");
+btnAll.addEventListener("click", function () {
+
+    getAll(all).then(data => {
+
+        data.forEach(item => {
+            console.log(item)
+            let { img, name, nickname, occupation } = item;
+            let card = createCard(img, name, nickname, occupation)
+            appendCard(card);
+        })
+
+    }).catch(error => {
+        console.log(error);
+    });
+})
+
+//GET CHARACTER BY NAME-----------------------------------------------------------
+
+async function getName(name) {
+    const requestURL = 'https://www.breakingbadapi.com/api/characters?name=' + name;
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btnName = document.getElementById("name");
+btnName.addEventListener("click", function () {
+    let inputName = prompt("Enter character name: ", "Walter")
+
+    getName(inputName).then(data => {
+        data.forEach(item => {
+            console.log(item)
+            let { img, name, nickname, occupation } = item;
+            let card = createCard(img, name, nickname, occupation)
+            appendCard(card);
+        })
+
+    }).catch(error => {
+        console.log(error);
+    });
+})
+
+//GET CERTAIN COUNT------------------------------------------------------------------
+
+async function getCount(count) {
+    const requestURL = 'https://www.breakingbadapi.com/api/characters?limit=' + count;
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btnCount = document.getElementById("count");
+btnCount.addEventListener("click", function () {
+    let inputCount = prompt("Enter the count of characters: ", "10")
+
+    getCount(inputCount).then(data => {
+        data.forEach(item => {
+            console.log(item)
+            let { img, name, nickname, occupation } = item;
+            let card = createCard(img, name, nickname, occupation)
+            appendCard(card);
+        })
+
+    }).catch(error => {
+        console.log(error);
+    });
+})
+
+//ALL CHARACTERS FROM "BETTER CALL SAUL"------------------------------------------------------------
+
+async function getAll2() {
+    const requestURL = 'https://www.breakingbadapi.com/api/characters?category=Better+Call+Saul';
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btnAll2 = document.getElementById("saul");
+btnAll2.addEventListener("click", function () {
+
+    getAll2(all).then(data => {
+
+        data.forEach(item => {
+            console.log(item)
+            let { img, name, nickname, occupation } = item;
+            let card = createCard(img, name, nickname, occupation)
+            appendCard(card);
+        })
+
+    }).catch(error => {
+        console.log(error);
+    });
+})
+
+//FILTER CHARACTERS BY SEASON-------------------------------------------------------------------
+
+async function getSeason() {
+    const requestURL = 'https://www.breakingbadapi.com/api/characters?category=breaking+bad';
+    const request = new Request(requestURL);
+    const response = await fetch(request, { mode: 'cors' });
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+}
+
+let btnSeason = document.getElementById("season");
+btnSeason.addEventListener("click", function () {
+    let inputSeason = +prompt("Enter season number: ");
+
+    getSeason(inputSeason).then(data => {
+        let filtered = data.filter(item => item.appearance.includes(inputSeason));
+        console.log(filtered);
+        filtered.forEach(item => {
+            let { img, name, nickname, occupation } = item;
+            let card = createCard(img, name, nickname, occupation);
+            appendCard(card);
+        })
+
+    }).catch(error => {
+        console.log(error);
+    })
 })
